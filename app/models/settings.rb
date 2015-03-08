84d05e0435882d36f775414e0f04e929945bef1f
Sara <sara@mavenhive.in>
2015-03-08 21:42:31 +0530

    Remove unused settings

--- app/models/settings.rb
+++ app/models/settings.rb
@@ -2,8 +2,4 @@ class Settings < Settingslogic
   source "#{Rails.root}/config/application.yml.erb"
   namespace Rails.env
   load!
-
-  def self.grasshopper_account_features_url
-    "#{Settings.grasshopper_mailer_url}/#/settings/plan"
-  end
 end
